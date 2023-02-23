15b25618c52785a97ffc1028143acdd8ae3dd313
Kamingos <gooaal.j@mail.ru>
2023-02-23 21:31:10 +0700

    Change2
    Main UI Complete
    Day 2

--- AndroidStudio/app/src/main/java/com/example/case_android_dev/MainActivity.java
+++ AndroidStudio/app/src/main/java/com/example/case_android_dev/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.case_android_dev;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Debug;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+
+    }
 }
